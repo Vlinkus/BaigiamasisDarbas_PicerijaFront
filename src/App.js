39fa228eaa5f85649610179f51a50933e13240f9
Maksim Pavlenko <maksimas.pavlenko@gmail.com>
2023-07-25 16:04:37 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//import background from './pic/bg1.svg';
+import ExampleComponent from './ExampleComponent';
 import './App.css';
-
+//<div style={{height:'48px',width:'auto',float: 'right'}}>
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="top_mobile_header">
+          <label>Pizzeriaa</label>
+          
+            <div className="login_icon"></div>
+            <div className="cart_icon" ></div>
+            <div className="help_icon" ></div>
+
+        </div>
       </header>
+      <div className="content_body" >
+        <ExampleComponent />
+      </div>
+      <div className="bottom_footer">das</div>
     </div>
   );
 }
