a0d6df2290fe168cfd30d342f5d59cf87a3868fa
Gytis <gytispaskauskas@yahoo.com>
2023-07-28 16:14:49 +0300

    Added Picos Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 //import background from './pic/bg1.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AppNavbar from './components/AppNavBar';
-import NotFound from './components/NotFound';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import ExampleComponent from './components/ExampleComponent';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppNavbar from "./components/AppNavBar";
+import NotFound from "./components/NotFound";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import ExampleComponent from "./components/ExampleComponent";
+import PicosPage from "./PicosPage";
+import "./PicosPage.css";
 
 function App() {
   return (
     <Router>
-    <AppNavbar />
-      <div className="main" >
+      <AppNavbar />
+      <div className="main">
         <Switch>
           <Route path="/" exact component={ExampleComponent} />
-          <Route path="/picos" exact component={ExampleComponent} />
+          <Route path="/picos" exact component={PicosPage} />
           <Route path="/*" exact component={NotFound} />
         </Switch>
       </div>
-    <Footer />
-  </Router>
+      <Footer />
+    </Router>
   );
 }
 
