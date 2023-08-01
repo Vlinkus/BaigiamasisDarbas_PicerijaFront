3cbf8c6f858e0d8ebdb0e5769b9fc1da92055c23
Maximas <maksimas.pavlenko@gmail.com>
2023-08-01 09:55:57 +0300

    new login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,16 +2,17 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
     return (
-        <>
-        <h3>el. paštas</h3>
-        <input type="text"/>
+        <div style={{maxWidth:"24em"}}>
+        <label>El. paštas *</label>
+        <input type="email" placeholder="El. paštas" className="p_input-field" required/>
+        <label>Slaptažodis *</label>
+        <input type="email" placeholder="Slaptažodis" className="p_input-field" required/>
+        <input type="checkbox" id="iagree" value="Prisiminti mane"/>
+        <label for="iagree">Prisiminti mane</label><br/>
+        <input type="button" value="Prisijungti" className="p_button"/><br/>
+        <Link to="#">Pamiršai slaptažodį?</Link><br/>
 
-        <h3>slaptažodis</h3>
-        <input type="text"/>
-        <input type="checkbox" value="Prisiminti mane"/>
-        <input type="button" value="Prisijungti"/><br/>
-        <input type="button" value="Registruotis"/><br/>
-        <Link to="#">Pamiršai slaptažodį?</Link>
-        </>
+        
+        </div>
     );
 }
