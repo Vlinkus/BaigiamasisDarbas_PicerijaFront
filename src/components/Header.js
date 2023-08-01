b89ecb5c8f562f5fd77a054372e23a88fc677969
VLinkus <LinkusVyte@gmail.com>
2023-08-01 11:45:04 +0300

    Changed class= to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,25 +8,25 @@ import pizzeria from "./Images/pizzeria.jpg"
 export default function Header() {
     return (
         <>
-            <div class="p_navbar_wrapper">
-                <div class="p_navbar_">
-                    <section class="p_left-items">
+            <div className="p_navbar_wrapper">
+                <div className="p_navbar_">
+                    <section className="p_left-items">
                         <Link to="/">
                             <img src={pizzeria} style={{height:"3.5em"}}/>
                         </Link>
                         <Link to="/picos">
-                            <div class="p_nav-item">Picos</div>
+                            <div className="p_nav-item">Picos</div>
                         </Link>
                         <Link to="/404">
-                            <div class="p_nav-item">Kontaktai</div>
+                            <div className="p_nav-item">Kontaktai</div>
                         </Link>
                     </section>
-                    <section class="p_right-items">
+                    <section className="p_right-items">
                         <Link to="/login">
-                            <div class="p_nav-item">Prisijungti</div>
+                            <div className="p_nav-item">Prisijungti</div>
                         </Link>
                         <Link to="/register">
-                            <div class="p_nav-item">Registruotis</div>
+                            <div className="p_nav-item">Registruotis</div>
                         </Link>
                     </section>
                 </div>
