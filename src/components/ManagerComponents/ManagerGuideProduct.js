3dda996e31d4c8971b1cb39a44604b5addf0250c
VLinkus <LinkusVyte@gmail.com>
2023-08-18 01:09:08 +0300

    Manager Guide Completed

--- src/components/ManagerComponents/ManagerGuideProduct.js
+++ src/components/ManagerComponents/ManagerGuideProduct.js
@@ -1,52 +1,46 @@
 import { useTranslationAndLanguageChange } from "../TranslationComponents/TranslationUtils";
 
-export default function ManagerGuidePizza() {
+export default function ManagerGuideProduct() {
     const { t, changeLanguageHandler } = useTranslationAndLanguageChange();
     return (
         <div>
             <ul className="managerGuidInstructionsList">
                 <li>
-                    <h2>{t("Adding New Pizza")}</h2>
-                    <p>{t("At the top of Pizza page there is button")}   <button className="btn btn-success">{t("Add New Pizza")} </button>
-                        {t("Clicked button opens new Pizza creation window. Fill all fields and Press SUBMIT. New Pizza will be created.")}
+                    <h2>{t("Adding New Product")}</h2>
+                    <p>{t("At the top of Products page there is button")}   <button className="btn btn-success">{t("Add New Product")} </button>
+                        {t("Clicked button opens new Product creation window. Fill all fields and Press SUBMIT. New Product will be created.")}
                     </p>
                 </li>
                 <li>
-                    <h2>{t("Managment of Pizza List Page")}</h2>
-                    <p> {t("Bellow you can see pizza list in a table")}</p>
+                    <h2>{t("Managment of Products List Page")}</h2>
+                    <p> {t("Bellow you can see products list in a table")}</p>
                     <table className="table table-hover table-striped">
                         <thead>
                             <tr>
                                 <th>{t("Line Number")}</th>
-                                <th>{t("Pizza Name")}</th>
-                                <th>{t("Pizza Picture")}</th>
+                                <th>{t("Product Name")}</th>
                                 <th>{t("Unit Price")}</th>
-                                <th>{t("Pizza Size")}</th>
-                                <th>{t("Products")}</th>
                                 <th>{t("Actions")}</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr >
-                                <td>1</td>
-                                <td>Margarita</td>
-                                <td> {t("Pizza Picture")} </td>
-                                <td> 15 €</td>
-                                <td> 44 </td>
-                                <td> {t("Mozzarella")}<br /> {t("Spinach")}<br /> {t("Tomato pasta sauce")} </td>
+                                <td> 1 </td>
+                                <td> {t("Mozzarella")} </td>
+                                <td> 3.1 €</td>
                                 <td>
                                     <button type="button" className="btn btn-warning"> {t("Update")} </button>
                                     <button type="button" className="btn btn-danger" > {t("Delete")} </button>
                                 </td>
                             </tr>
-                            <tr><td scope="row">2</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td></tr>
+                            <tr><td scope="row">2</td><td>...</td><td>...</td><td>...</td></tr>
                         </tbody>
                     </table>
                     <p>{t("In table column Actions you can see buttons")} <br />
                         <button type="button" className="btn btn-warning"> {t("Update")} </button>
                         <button type="button" className="btn btn-danger" > {t("Delete")} </button> <br />
-                        {t("Clicked Update button opens Pizza update window. Change all required fields and Press SUBMIT. Pizza will be updated.")}<br />
-                        {t("Clicked Delete button will delete Pizza from table")}
+                        {t("Clicked Update button opens")} {t("Product update window.")} {t("Change all required fields and Press SUBMIT.")} {t("Product will be updated.")}<br />
+                        {t("Clicked Delete button will delete Product from table")}
                     </p>
                 </li>
             </ul>
